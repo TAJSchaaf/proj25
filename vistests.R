1025f93e81e06bd7d7ac9c8cf9e7c9fb51ebb8ed
TAJSchaaf <157992880+TAJSchaaf@users.noreply.github.com>
2025-01-20 00:40:04 +0100

    Aesthetics, About
    - removed navbar kram
    - added about page
    - added instruction boxes to edition and visualisations
    - changed reference colors

--- vistests.R
+++ vistests.R
@@ -16,21 +16,26 @@ indexfm$reference_type <- as.factor(indexfm$reference_type)
 indexfm <- indexfm[order(indexfm$title),] 
 
 ui <- bs4DashPage(
-  title = "Project 2025 Index",
-  header = bs4DashNavbar(),
+  title = "Project 2025 Index Visualiser",
+  help = NULL, # remove help checkbox
+  dark = NULL, # remove dark/light skins
+  header = bs4DashNavbar(
+    title = "Project 2025 Index Visualiser",
+    fixed = TRUE
+  ),
   sidebar = bs4DashSidebar(
     skin = "light",
     title = "Menu",
     collapsed = FALSE,
     bs4SidebarMenu(
       bs4SidebarMenuItem(
-        "General Visualizations", tabName = "general", icon = icon("chart-bar")
+        "Visualizations", tabName = "general", icon = icon("chart-bar")
       ),
       bs4SidebarMenuItem(
-        "Result Edition", tabName = "edition", icon = icon("book")
+        "Reference Results", tabName = "edition", icon = icon("book")
       ),
       bs4SidebarMenuItem(
-        "API Definitions", tabName = "api", icon = icon("info-circle")
+        "About Project", tabName = "about", icon = icon("info-circle")
       ),
       
       actionButton(inputId = "confirm_button", label = "Confirm Selection"),
@@ -46,35 +51,68 @@ ui <- bs4DashPage(
   body = bs4DashBody(
     tags$head(
       tags$script(src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"),
-      tags$script(src = "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js")
+      tags$script(src = "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"),
+      tags$style(HTML("
+        body {
+          zoom: 1.1; /* Adjust the zoom level to make everything appear larger */
+        }
+        .content-wrapper {
+          padding: 10px; /* Reduce padding if needed */
+        }
+      "))
     ),
     bs4TabItems(
-      # General Visualizations Tab
+      # Visualizations Tab
       bs4TabItem(
         tabName = "general",
-        h2("General Visualizations"),
-        tableOutput("title_stats_table"),
+        h2("Project 2025 Index Visualiser"),
+        fluidRow(
+          column(
+            width = 12,
+            box(
+              width = NULL,
+              title = "How to use this website",
+              "Select the keywords you want to explore using the dropdown menu or checkboxes, and click the Confirm button to load related data.",br(), br(),
+              "View keywords by chapter section on the heatmap, examine the statistics related to keyword use in the table, and compare the frequency of reference types in the bar chart.", br(), br(),
+              "To view specific references within the document, go to the Reference Results tab in the sidebar."
+            ),
+          )
+        ),
+        h3("Frequency of Keywords per Chapter"),
         plotOutput("heatmap"),
+        
+        h3("Statistics of Keyword Use"),
+        tableOutput("title_stats_table"),
+        
+        h3("Frequency of Keyword Reference Types"),
         plotOutput("bar_chart")
       ),
       # Edition Tab
       bs4TabItem(
         tabName = "edition",
-        h2("Edition"),
+        h2("Reference Results"),
         # Add this new filtering section
         fluidRow(
           column(
             width = 12,
             box(
               width = NULL,
-              title = "Filter by Reference Type",
-              checkboxGroupInput(
-                inputId = "reference_type_filter",
-                label = NULL,
-                choices = c("Statement", "Critique", "Recommendation", "Other"),
-                selected = c("Statement", "Critique", "Recommendation", "Other"),
-                inline = TRUE
-              )
+              title = "How to use Reference Results",
+              HTML("On this page, you can view the context in which selected keywords are used within the document, organized by chapter section. Opening a chapter section reveals the selected keywords used in that chapter, along with an AI-generated summary of their usage.
+                   </br></br>
+                   Clicking on a page number will take you directly to the corresponding page in the Project 2025 document. Additionally, you can sort and filter the results by reference type: 
+                      <span style='background-color: #ffffb0; font-weight: bold;'>Statement</span>,
+                      <span style='background-color: #ff7891; font-weight: bold;'>Critique</span>, 
+                      <span style='background-color: #b0ffff; font-weight: bold;'>Recommendation</span>,  or 
+                      <span style='background-color: #b0b0b0; font-weight: bold;'>Other</span>.
+                    This helps you quickly locate and analyze the most relevant information.")
+            ),
+            checkboxGroupInput(
+              inputId = "reference_type_filter",
+              label = "Filter by Reference Type",
+              choices = c("Statement", "Critique", "Recommendation", "Other"),
+              selected = c("Statement", "Critique", "Recommendation", "Other"),
+              inline = TRUE
             )
           )
         ),
@@ -82,12 +120,19 @@ ui <- bs4DashPage(
       ),
       # API Definitions Tab
       bs4TabItem(
-        tabName = "api",
+        tabName = "about",
         h2("API Definitions"),
-        textOutput("about_info")
+        HTML("This project was developed with the aim of improving the accessibility and analysis of the Project 2025 document by providing an intuitive way to explore its contents based on specific keywords. The primary goal of this tool is to allow users to quickly identify the context in which selected keywords appear throughout the document, organized by sections. By utilizing an interactive heatmap, reference filters, and detailed summaries, this app facilitates the identification and analysis of important references, enabling users to gain deeper insights into the document’s structure and key themes. This tool is particularly useful for those looking to focus on particular topics and understand their significance within the larger context of the document.
+             </br></br>
+              The application was designed to provide an interactive, user-friendly experience with features such as keyword filtering, page navigation, and customizable results views. This makes it easier for users to navigate the document, find relevant sections, and examine the relationships between references and specific keywords. Whether the user is conducting a thorough review or simply seeking a quick overview, this tool is a valuable resource for efficiently parsing the extensive content of the Project 2025 document.
+            </br></br>
+              I would like to express my heartfelt gratitude to the original producers of the code, the Autonomy Data Unit, and the various resources used in the development of this project. Their contributions laid the foundation for the functionality that drives this app. 
+             </br></br>
+             A special thanks goes to the individuals who provided assistance, feedback, and guidance during the development process. Their support has been crucial in refining the app and ensuring that it meets the needs of users. Without their collaboration, this project would not have come to fruition.")
       )
     )
-  )
+  ),
+  footer = bs4DashFooter("Thea Schaaf 2025")
 )
 
 server <- function(input, output, session) {
@@ -143,13 +188,15 @@ server <- function(input, output, session) {
     # Format the percentages and total values together for each reference type
     stats_table <- stats_table %>%
       mutate(
-        sections = paste(total_sections, "/36", " (", sections_percentage, "%)", sep = ""),
-        statement = paste(total_statements, " (", statement_percentage, "%)", sep = ""),
-        critique = paste(total_critique, " (", critique_percentage, "%)", sep = ""),
-        recommendation = paste(total_recommendation, " (", recommendation_percentage, "%)", sep = ""),
-        other = paste(total_other, " (", other_percentage, "%)", sep = "")
+        Keyword = title,
+        Total = total_references,
+        Sections = paste(total_sections, " of 36", " (", sections_percentage, "%)", sep = ""),
+        Statements = paste(total_statements, " (", statement_percentage, "%)", sep = ""),
+        Crits = paste(total_critique, " (", critique_percentage, "%)", sep = ""),
+        Recs = paste(total_recommendation, " (", recommendation_percentage, "%)", sep = ""),
+        Other = paste(total_other, " (", other_percentage, "%)", sep = "")
       ) %>%
-      select(-total_sections, -sections_percentage, -total_statements, -statement_percentage, -total_critique, -critique_percentage,
+      select(-title, -total_references, -total_sections, -sections_percentage, -total_statements, -statement_percentage, -total_critique, -critique_percentage,
              -total_recommendation, -recommendation_percentage, -total_other, -other_percentage)  # Remove intermediate columns
     
     # Return the formatted table
@@ -161,7 +208,7 @@ server <- function(input, output, session) {
   output$heatmap <- renderPlot({
     titles <- selected_titles()
     if (is.null(titles) || length(titles) == 0) {
-      plot(1, type = "n", xlab = "", ylab = "", main = "Please select titles and click Confirm")
+      plot(1, type = "n", xlab = "", ylab = "", main = "Please select keywords in sidebar.")
       return()
     }
     
@@ -181,12 +228,13 @@ server <- function(input, output, session) {
     ggplot(complete_data, aes(x = section_simple, y = reorder(title, n), fill = n)) +
       geom_tile(color = "black") +
       scale_fill_gradient(low = "white", high = "red") +
-      labs(title = "Heatmap of Title Occurrences by Section", x = "Section", y = "Title", fill = "Count") +
+      labs(title = "Keyword Occurrences by Chapter Section", x = NULL, y = NULL, fill = "Frequency") +
       theme_minimal() +
       theme(
         axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate section names for better readability
-        plot.margin = margin(0, 0, 0, 0)  # Remove any extra margin around the plot
-      )
+        strip.text = element_blank() 
+      ) +
+      coord_fixed(ratio = 3)  # height of cells
   })
   
   # Stacked Bar Chart Visualization
@@ -204,14 +252,14 @@ server <- function(input, output, session) {
     ggplot(filtered_data, aes(x = reorder(title, n), y = n, fill = reference_type)) +
       geom_bar(stat = "identity", position = "stack") +
       labs(
-        title = "Stacked Bar Chart of Title Frequency by Reference Type",
+        title = "Keyword Frequency by Reference Type",
         x = "Title", y = "Frequency", fill = "Reference Type"
       ) +
       scale_fill_manual(values = c(
-        "Statement" = "#708d81", 
-        "Critique" = "#bc4b51", 
-        "Recommendation" = "#f4d58d", 
-        "Other" = "#e0e1dd"
+        "Statement" = "#fce61a", 
+        "Critique" = "#ff7891", 
+        "Recommendation" = "#b0ffff", 
+        "Other" = "#b0b0b0"
       )) +
       coord_flip() +
       theme_minimal()
@@ -242,10 +290,10 @@ server <- function(input, output, session) {
     
     # Define a mapping of reference_type to colors
     reference_colors <- c(
-      "Statement" = "#bc4b51",
-      "Critique" = "#e0e1dd",
-      "Recommendation" = "#f4d58d",
-      "Other" = "#c68a8a"
+      "Critique" = "#ff7891",
+      "Other" = "#b0b0b0",
+      "Recommendation" = "#b0ffff",
+      "Statement" = "#fce61a"
     )
     
     # Create accordion wrapper
@@ -268,12 +316,12 @@ server <- function(input, output, session) {
           ref_color <- reference_colors[ref$reference_type]
           
           sprintf(
-            "<div style='background-color: %s; padding: 10px; margin-bottom: 5px; border-radius: 5px;'>
-             <b>Title:</b> %s<br>
-             <b>Page:</b> %d<br>
-             <b>Summary:</b> %s
-           </div>",
-            ref_color, ref$title, ref$page, ref$summary
+            "<div style='background-color: %s; padding: 10px; margin-bottom: 5px; border-radius: 5px; display: flex; justify-content: space-between; align-items: center;'>
+      <span style='font-size: 1.25em;'><b>%s</b></span>
+      <span style='font-size: 1.25em;'><b><a href='https://archive.org/details/2025_MandateForLeadership_FULL/page/%d/mode/2up' target = '_blank'>%d.</a></b></span> <!-- page number -->
+    </div>
+    <span>%s</span></br></br>", 
+              ref_color, ref$title, ref$page, ref$page, ref$summary
           )
         }) %>%
           unlist() %>%
@@ -290,9 +338,10 @@ server <- function(input, output, session) {
                 class = "btn btn-link btn-block text-left",
                 `data-toggle` = "collapse",
                 `data-target` = paste0("#", section_id),
-                `aria-expanded` = "true",
+                `aria-expanded` = "false",  # This will toggle dynamically
                 `aria-controls` = section_id,
-                paste(section_name, "(", nrow(references), "references)")
+                paste(current_section_index, ": ", section_name, "(", nrow(references), " references)"),
+                style = "font-size: .7em; color: black; border-radius: 5px;"  # Custom style
               )
             )
           ),
@@ -308,6 +357,7 @@ server <- function(input, output, session) {
           )
         )
       })
+      
     ))
   })
   
